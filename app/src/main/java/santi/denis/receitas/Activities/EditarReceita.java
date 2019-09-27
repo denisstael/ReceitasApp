1b9e6f8deac276afac39dff390dc51d924da498f
Denis Stael <denisstael@gmail.com>
2019-09-26 22:55:41 -0300

    Project V.2

--- app/src/main/java/santi/denis/receitas/Activities/EditarReceita.java
+++ app/src/main/java/santi/denis/receitas/Activities/EditarReceita.java
@@ -1,39 +1,60 @@
 package santi.denis.receitas.Activities;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import santi.denis.receitas.R;
 import santi.denis.receitas.db.BancoController;
+import santi.denis.receitas.db.CriaBanco;
 
-public class NovaReceita extends AppCompatActivity {
+public class EditarReceita extends AppCompatActivity {
 
-    Button button;
+    String codigo = null;
+    BancoController bancoController;
+    TextView title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.nova_receita);
+        codigo = this.getIntent().getStringExtra("codigo");
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
-        setContentView(R.layout.nova_receita);
 
-        button = findViewById(R.id.button2);
-        button.setText(R.string.cancelar);
+        title = findViewById(R.id.tv_title);
+        title.setText(R.string.editar_receita);
+
+        if (codigo != null) {
+            bancoController = new BancoController(getBaseContext());
+            EditText nome = findViewById(R.id.editText);
+            EditText ingredientes = findViewById(R.id.editText2);
+            EditText modo_preparo = findViewById(R.id.editText3);
+            TextView id = findViewById(R.id.textView);
+            Cursor cursor = bancoController.carregaDadosById(Integer.parseInt(codigo));
+            id.setText(cursor.getString(cursor.getColumnIndexOrThrow(CriaBanco.ID)));
+            nome.setText(cursor.getString(cursor.getColumnIndexOrThrow(CriaBanco.NOME)));
+            ingredientes.setText(cursor.getString(cursor.getColumnIndexOrThrow(CriaBanco.INGREDIENTES)));
+            modo_preparo.setText(cursor.getString(cursor.getColumnIndexOrThrow(CriaBanco.MODO_PREPARO)));
+        }
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) { //Botão adicional na ToolBar
         switch (item.getItemId()) {
             case android.R.id.home:  //ID do seu botão (gerado automaticamente pelo android, usando como está, deve funcionar
-                startActivity(new Intent(this, MainActivity.class));  //O efeito ao ser pressionado do botão (no caso abre a activity)
+                Intent intent = new Intent(this, Receitas.class);
+                Bundle params = new Bundle();
+                params.putString("codigo", codigo);
+                intent.putExtras(params);
+                startActivity(intent);  //O efeito ao ser pressionado do botão (no caso abre a activity)
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
                     finishAffinity();  //Método para matar a activity e não deixa-lá indexada na pilhagem
                 }
@@ -68,6 +89,11 @@ public class NovaReceita extends AppCompatActivity {
     }
 
     public void onClickExcluir(View view) {
+        TextView id = findViewById(R.id.textView);
+        String idString = id.getText().toString();
+        BancoController bancoController = new BancoController(getBaseContext());
+        String resultado = bancoController.excluiReceita(idString);
+        Toast.makeText(getBaseContext(), resultado, Toast.LENGTH_SHORT).show();
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
--- app/src/main/java/santi/denis/receitas/Activities/Receitas.java
+++ app/src/main/java/santi/denis/receitas/Activities/Receitas.java
@@ -1,8 +1,13 @@
 package santi.denis.receitas.Activities;
 
+import android.content.Intent;
 import android.database.Cursor;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import santi.denis.receitas.R;
 import santi.denis.receitas.db.BancoController;
@@ -12,13 +17,18 @@ public class Receitas extends AppCompatActivity {
 
     String codigo = null;
     BancoController bancoController;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.receitas);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
         codigo = this.getIntent().getStringExtra("codigo");
+        button = findViewById(R.id.editar);
 
         if (codigo != null) {
             bancoController = new BancoController(getBaseContext());
@@ -30,5 +40,30 @@ public class Receitas extends AppCompatActivity {
             ingredientes.setText(cursor.getString(cursor.getColumnIndexOrThrow(CriaBanco.INGREDIENTES)));
             modo_preparo.setText(cursor.getString(cursor.getColumnIndexOrThrow(CriaBanco.MODO_PREPARO)));
         }
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), EditarReceita.class);
+                Bundle params = new Bundle();
+                params.putString("codigo", codigo);
+                intent.putExtras(params);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) { //Botão adicional na ToolBar
+        switch (item.getItemId()) {
+            case android.R.id.home:  //ID do seu botão (gerado automaticamente pelo android, usando como está, deve funcionar
+                startActivity(new Intent(this, MainActivity.class));  //O efeito ao ser pressionado do botão (no caso abre a activity)
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                    finishAffinity();  //Método para matar a activity e não deixa-lá indexada na pilhagem
+                }
+                break;
+            default:break;
+        }
+        return true;
     }
 }
