e2f8f0d6a8802fa8bcf5f7b9a56198e3d52e52e4
Denis Stael <denisstael@gmail.com>
2019-09-27 12:13:22 -0300

    Improvements

--- app/src/main/java/santi/denis/receitas/Activities/MainActivity.java
+++ app/src/main/java/santi/denis/receitas/Activities/MainActivity.java
@@ -1,9 +1,12 @@
 package santi.denis.receitas.Activities;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -38,7 +41,6 @@ public class MainActivity extends AppCompatActivity {
                 String codigo;
                 cursor.moveToPosition(position);
                 codigo = cursor.getString(cursor.getColumnIndexOrThrow(CriaBanco.ID));
-
                 Intent intent = new Intent(getBaseContext(), Receitas.class);
                 Bundle params = new Bundle();
                 params.putString("codigo", codigo);
@@ -46,6 +48,33 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        //Handle the back button
+        if(keyCode == KeyEvent.KEYCODE_BACK) {
+            //Ask the user if they want to quit
+            new AlertDialog.Builder(this)
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .setTitle(R.string.sair)
+                    .setMessage(R.string.realmente_sair)
+                    .setPositiveButton(R.string.sim, new DialogInterface.OnClickListener() {
+
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            //Stop the activity
+                            MainActivity.this.finish();
+                        }
+                    })
+                    .setNegativeButton(R.string.nao, null)
+                    .show();
+
+            return true;
+        }
+        else {
+            return super.onKeyDown(keyCode, event);
+        }
     }
 
     public void onClickAdd(View view) {
--- app/src/main/java/santi/denis/receitas/Activities/Receitas.java
+++ app/src/main/java/santi/denis/receitas/Activities/Receitas.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 import santi.denis.receitas.R;
